77a54d68067cfc7e923f1c945371e0a043152a1d
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-13 15:26:56 +0530

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -5,42 +5,35 @@ s3_bucket="nodemode"
 current_date=$(date '+%Y-%m-%d')
 output_file="services_$current_date.txt"
 
-# Create an empty file
-> "$output_file"
+# Check if a service name is provided as an argument
+if [ -z "$1" ]; then
+  echo "No service name provided."
+  exit 1
+fi
 
-echo "Listing all services with desired count 1..."
+service="$1"
 
-aws ecs list-services --cluster "$cluster" | awk -F'"' '{print $2}' | rev | awk -F'/' '{print $1}' | rev | grep -v serviceArns > all_services.txt
+desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service" --query "services[0].desiredCount" --output text)
 
-services=$(cat all_services.txt)
-pids=()
+if [ "$desiredCount" -eq 1 ]; then
+  # Check if the service is already recorded in the file
+  if ! grep -q "$service" "$output_file"; then
+    # Scale down the service
+    aws ecs update-service --cluster "$cluster" --service "$service" --desired-count 0 --no-cli-pager > /dev/null
+    aws ecs wait services-stable --cluster "$cluster" --services "$service"
 
-for service in $services; do
-  (
-    desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service" --query "services[0].desiredCount" --output text)
+    # Log updated service
+    echo "$service" >> "$output_file"
+    echo "Scaled down and stabilized service: $service"
 
-    if [ "$desiredCount" -eq 0 ]; then
-      # Check if the service is already recorded in the file
-      if ! grep -q "$service" "$output_file"; then
-        # Scale down the service
-        aws ecs update-service --cluster "$cluster" --service "$service" --desired-count 1 --no-cli-pager > /dev/null
-        aws ecs wait services-stable --cluster "$cluster" --services "$service"
-        aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
+    # Upload the updated services file to S3
+    aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
+    echo "File uploaded to S3: $s3_bucket/$output_file"
+  else
+    echo "Service $service is already recorded in the file. Skipping upload."
+  fi
+else
+  echo "Service $service is not running with desired count 1."
+fi
 
-        # Log updated service
-        echo "$service" >> "$output_file"
-        echo "Scaled down and stabilized service: $service"
-      fi
-    fi
-  ) &
-  pids+=("$!")
-done
-
-# Wait for all background processes to complete
-for pid in "${pids[@]}"; do
-  wait "$pid"
-done
-
-
-#echo "File uploaded to S3: $s3_bucket/$output_file"
-#echo "Process completed. Services saved to $output_file."
+echo "Process completed for service: $service."
