98ea6ebb80ef0ed00aed08f31246a9c75782f964
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-13 13:37:35 +0530

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
-# Define your ECS cluster name
 cluster="my_dev_cluster37"
+s3_bucket="nodemode"
+current_date=$(date '+%Y-%m-%d')
+output_file="services_$current_date.txt"
 
-# Clear previous services.txt file
-> services.txt
+> "$output_file"
+echo "Listing all services with desired count 1..."
 
-echo "Listing all services with desired count 1 and saving to services.txt..."
-
-# List all services in the cluster and save to file
 aws ecs list-services --cluster "$cluster" | awk -F'"' '{print $2}' | rev | awk -F'/' '{print $1}' | rev | grep -v serviceArns > all_services.txt
 
 services=$(cat all_services.txt)
-
-pids=()  # Store process IDs
+pids=()
 
 for service in $services; do
   (
-    # Get the desired count
     desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service" --query "services[0].desiredCount" --output text)
 
-    if [ "$desiredCount" -eq 0 ]; then
-      echo "$service" >> services.txt
-      echo "Service $service has desired count 1."
-
-      # Scale down the service and wait for stabilization
-      aws ecs update-service --cluster "$cluster" --service "$service" --desired-count 1 --no-cli-pager > /dev/null
-
+    if [ "$desiredCount" -eq 1 ]; then
+      echo "$service" >> "$output_file"
+      aws ecs update-service --cluster "$cluster" --service "$service" --desired-count 0 --no-cli-pager > /dev/null
       aws ecs wait services-stable --cluster "$cluster" --services "$service"
-
       echo "Scaled down and stabilized service: $service"
     fi
-  ) &  # Run in the background
-
-  pids+=("$!")  # Store the process ID
-
+  ) &
+  pids+=("$!")
 done
 
-# Wait for all background processes to complete
 for pid in "${pids[@]}"; do
   wait "$pid"
 done
 
-echo "Process completed. Services with desired count 1 saved to services.txt."
+echo "Process completed. Services saved to $output_file."
+aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
+echo "File uploaded to S3: $s3_bucket/$output_file"
