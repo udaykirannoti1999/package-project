445f4e98848bb58fb43e8f27a85a259b8e0be6c9
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-13 14:59:46 +0530

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -5,7 +5,8 @@ s3_bucket="nodemode"
 current_date=$(date '+%Y-%m-%d')
 output_file="services_$current_date.txt"
 
-touch "$output_file"
+# Create an empty file to collect services
+> "$output_file"
 
 echo "Listing all services with desired count 1..."
 
@@ -19,25 +20,26 @@ for service in $services; do
     desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service" --query "services[0].desiredCount" --output text)
 
     if [ "$desiredCount" -eq 1 ]; then
-     
-      #echo "$service" >> "$output_file"
+      # Add service to the file
+      echo "$service" >> "$output_file"
 
+      # Scale down the service
       aws ecs update-service --cluster "$cluster" --service "$service" --desired-count 0 --no-cli-pager > /dev/null
       aws ecs wait services-stable --cluster "$cluster" --services "$service"
-      aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
       
-      #echo "File uploaded to S3: $s3_bucket/$output_file"
-
       echo "Scaled down and stabilized service: $service"
     fi
   ) &
   pids+=("$!")
 done
 
+# Wait for all background processes to complete
 for pid in "${pids[@]}"; do
   wait "$pid"
 done
 
+# Upload the file to S3 (only once)
+aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
+
+echo "File uploaded to S3: $s3_bucket/$output_file"
 echo "Process completed. Services saved to $output_file."
-
-
