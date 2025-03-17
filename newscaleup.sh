915020e944f3d3844b2273460a3324813f51d339
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-17 18:52:59 +0530

    Update newscaleup.sh

--- newscaleup.sh
+++ newscaleup.sh
@@ -2,17 +2,15 @@
 
 service_name=$1
 cluster="my_dev_cluster37"
-
-yesterday= $(date '+%Y-%m-%d')
-filePattern="services_${yesterday}.txt"
 s3_bucket="nodemode"
+
+currentDate=$(date +%Y-%m-%d)
+filePattern="services_${currentDate}.txt"
 
 if [ -z "$service_name" ]; then
   echo "Error: No service name provided."
   exit 1
 fi
-
-aws s3 ls s3://nodemode/
 
 # Download the scaling file from S3
 aws s3 cp s3://$s3_bucket/$filePattern . > /dev/null 2>&1
@@ -22,15 +20,24 @@ if [ ! -f "$filePattern" ]; then
   exit 1
 fi
 
-# Scale the service up if desired count is 0
-if [ "$service_name" != "$filePattern" ]; then
-    desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service_name" --query "services[0].desiredCount" --output text)
+# If the script is called with the scaling file name, return the list of services
+if [ "$service_name" == "$filePattern" ]; then
+    cat "$filePattern"
+    exit 0
+fi
 
-    if [ "$desiredCount" -eq 0 ]; then
-        aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 1 --no-cli-pager > /dev/null
-        aws ecs wait services-stable --cluster "$cluster" --services "$service_name"
-        echo "Service $service_name scaled up to desired count 1."
-    else
-        echo "Service $service_name is already running with desired count $desiredCount."
-    fi
+# Scale the service up if desired count is 0
+desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service_name" --query "services[0].desiredCount" --output text)
+
+if [ "$desiredCount" == "None" ]; then
+    echo "Error: Service $service_name not found in the cluster."
+    exit 1
+fi
+
+if [ "$desiredCount" -eq 0 ]; then
+    aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 1 --no-cli-pager > /dev/null
+    aws ecs wait services-stable --cluster "$cluster" --services "$service_name"
+    echo "Service $service_name scaled up to desired count 1."
+else
+    echo "Service $service_name is already running with desired count $desiredCount."
 fi
