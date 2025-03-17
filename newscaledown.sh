b212c8e0b621d4b5917d7d18e25165ffed09fc7d
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-17 12:08:46 +0530

    Update newscaledown.sh

--- newscaledown.sh
+++ newscaledown.sh
@@ -5,7 +5,7 @@ s3_bucket="nodemode"
 current_date=$(date '+%Y-%m-%d')
 output_file="services_$current_date.txt"
 service_name="$1"  # service name passed from Groovy
-slack_webhook_url=$(aws secretsmanager get-secret-value --secret-id 	myscreate234  --region ap-south-1 --query SecretString --output text | jq -r '.["slack-webhook"]')
+slack_webhook_url=$(aws secretsmanager get-secret-value --secret-id myscreate234  --region ap-south-1 --query SecretString --output text | jq -r '.["slack-webhook"]')
 
 # Clear previous content of the output file
 > "$output_file"
@@ -33,7 +33,7 @@ if [ "$desiredCount" -eq 1 ]; then
   aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 0 --no-cli-pager > /dev/null
   aws ecs wait services-stable --cluster "$cluster" --services "$service_name"
 
-  # checking whether duplicate services uploading or not
+  # Checking whether duplicate services are uploading or not
   if ! grep -qx "$service_name" "$output_file"; then
     echo "$service_name" >> "$output_file"
     echo "Service $service_name scaled down to desired count 0."
@@ -43,12 +43,15 @@ else
 fi
 
 # Upload the updated services file to S3
-aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"
-echo "File uploaded to S3: $s3_bucket/$output_file"
+if aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"; then
+  echo "File uploaded to S3: $s3_bucket/$output_file"
+  
+else
+  echo "Failed to upload file to S3."
 
- #Send notification to Slack
-curl -X POST -H 'Content-type: application/json' \
-     --data "{\"text\":\"Service scaling completed for: \`$service_name\`. File: \`$output_file\` uploaded to S3.\"}" "$slack_webhook_url"
-
+  # Send  notification to Slack
+  curl -X POST -H 'Content-type: application/json' \
+       --data "{\"text\":\"Service scaling for: \\`$service_name\\` failed to upload file to S3.\"}" "$slack_webhook_url"
+fi
 
 echo "Process completed for service: $service_name. Saved to $output_file."
