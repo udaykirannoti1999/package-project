19384e353fd3ed641e658c539607be8cd12a05af
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-18 11:33:54 +0530

    Update newscaleup.sh

--- newscaleup.sh
+++ newscaleup.sh
@@ -4,6 +4,7 @@ service_name=$1
 cluster="my_dev_cluster37"
 s3_bucket="nodemode"
 
+# Get the current date (yesterday's date)
 currentDate=$(date -d "yesterday" '+%Y-%m-%d')
 filePattern="services_${currentDate}.txt"
 
@@ -20,13 +21,13 @@ if [ ! -f "$filePattern" ]; then
   exit 1
 fi
 
-# If the script is called with the scaling file name, return the list of services
+# If the script is called without a service name, return the list of services
 if [ "$service_name" == "$filePattern" ]; then
     cat "$filePattern"
     exit 0
 fi
 
-# Scale the service up if desired count is 0
+# Check the desired count of the service
 desiredCount=$(aws ecs describe-services --cluster "$cluster" --services "$service_name" --query "services[0].desiredCount" --output text)
 
 if [ "$desiredCount" == "None" ]; then
@@ -35,6 +36,7 @@ if [ "$desiredCount" == "None" ]; then
 fi
 
 if [ "$desiredCount" -eq 0 ]; then
+    # Scale the service up to desired count 1
     aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 1 --no-cli-pager > /dev/null
     aws ecs wait services-stable --cluster "$cluster" --services "$service_name"
     echo "Service $service_name scaled up to desired count 1."
