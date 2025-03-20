91e0c0dee5b417e718e0ff901169a0315760f66a
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-03-20 16:28:05 +0530

    Update newscaledown.sh

--- newscaledown.sh
+++ newscaledown.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-cluster="my_dev_cluster37"
-s3_bucket="nodemode"
-current_date=$(date -d "yesterday" '+%Y-%m-%d')
-output_file="services_$current_date.txt"
+cluster="$2"
 service_name="$1"  # service name passed from Groovy
 slack_webhook_url=$(aws secretsmanager get-secret-value --secret-id myscreate234  --region ap-south-1 --query SecretString --output text | jq -r '.["slack-webhook"]')
-# Clear previous content of the output file
-> "$output_file"
 
 if [ -z "$service_name" ]; then
   echo "No service name provided. Exiting."
@@ -31,26 +26,9 @@ if [ "$desiredCount" -eq 1 ]; then
   # Update the service to desired count 0
   aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 0 --no-cli-pager > /dev/null
   aws ecs wait services-stable --cluster "$cluster" --services "$service_name"
-
-  # Checking whether duplicate services are uploading or not
-  if ! grep -qx "$service_name" "$output_file"; then
-    echo "$service_name" >> "$output_file"
-    echo "Service $service_name scaled down to desired count 0."
-  fi
+  
+  # Logging the result for Jenkins to read
+  echo "$service_name scaled down to desired count 0."
 else
   echo "Service $service_name already has desired count 0."
 fi
-
-# Upload the updated services file to S3
-if aws s3 cp "$output_file" "s3://$s3_bucket/$output_file"; then
-  echo "File uploaded to S3: $s3_bucket/$output_file"
-  
-else
-  echo "Failed to upload file to S3."
-
-  # Send  notification to Slack
-  curl -X POST -H 'Content-type: application/json' \
-       --data "{\"text\":\"Service scaling for: \\`$service_name\\` failed to upload file to S3.\"}" "$slack_webhook_url"
-fi
-
-echo "Process completed for service: $service_name. Saved to $output_file."
